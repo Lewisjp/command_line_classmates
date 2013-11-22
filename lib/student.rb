82306efde85c1b49b99193353322a23c20ee8442
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-11-22 08:45:05 -0500

    in progress

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/*'
 
 class Student 
 
@@ -8,11 +9,18 @@ class Student
 		@twitter = twitter
 		@blog = blog 
 
-
-
-
 	end
 
+	def all_data
+		all_data = Array.new
+		all_data << name << twitter << blog
+		all_data
+	end
 
+	#assuming we have a value for each category for each student
+	# def by_name_get_twitter
+
+
+	# end
 
 end
